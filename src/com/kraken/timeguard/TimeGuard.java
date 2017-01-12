2e8540f99e623484060f2a3f0935c85e1f73d34e
Randall Arms <contact.randallArms@gmail.com>
2017-01-11 23:16:24 -0800

    v2.0 | TimeGuard now works on all teleports
    - Added timeguard catch for all player teleport events.

--- src/com/kraken/timeguard/TimeGuard.java
+++ src/com/kraken/timeguard/TimeGuard.java
@@ -1,5 +1,5 @@
 // =========================================================================
-// |TIMEGUARD v1.3
+// |TIMEGUARD v2.0
 // | by Kraken | https://www.spigotmc.org/members/kraken_.287802/
 // | code inspired maxleovince95 & Strahan @ SpigotMC forums -- thank you.
 // |
